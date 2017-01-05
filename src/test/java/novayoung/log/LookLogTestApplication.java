32241deb140bb84a6d771bff548bdc4217a33acf
pengchangguo <347238>
2017-01-05 14:14:14 +0800

    Optimize

--- src/test/java/novayoung/log/LookLogTestApplication.java
+++ src/test/java/novayoung/log/LookLogTestApplication.java
@@ -1,7 +1,5 @@
 package novayoung.log;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
